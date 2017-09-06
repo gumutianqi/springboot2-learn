522804053de63562c54d2bcd0aeadcca180630dd
LarryKoo <gumutianqi@gmail.com>
2017-09-06 15:24:23 +0800

    新增 spring-boot2-case module

---
+++ spring-boot2-case/src/main/java/com/weteam/springboot2case/SpringBoot2CaseApplication.java
@@ -0,0 +1,12 @@
+package com.weteam.springboot2case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2CaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2CaseApplication.class, args);
+	}
+}
